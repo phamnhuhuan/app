5af76e5e6c8d7cd90ce5f543636cb085177c8b8d
baitapnhom12 <phuan3366@gmail.com>
2023-09-11 13:16:05 +0700

    app

---
+++ src/components/Admin/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+    
+    <div class="navbar" id="navigation">
+        <ul class="center-nav">
+            <RouterLink :to="{name:'admin-createnews'}"><li><i class="fa-sharp fa-solid fa-chart-simple"></i>Create News</li></RouterLink>
+            <RouterLink :to="{name:'admin-createtour'}"><li><i class="fa-sharp fa-solid fa-chart-simple"></i>Create Tour</li></RouterLink>
+            <RouterLink :to="{name:'admin-tour'}"><li><i class="fa-sharp fa-solid fa-chart-simple"></i>Tour</li></RouterLink>
+        </ul>
+        <ul class="bottom-nav">
+            <hr>
+            <li><i class="fa-solid fa-gear"></i><a href="#"> Settings </a></li>
+            <li><i class="fa-solid fa-power-off"></i><a href="#"> Logout </a></li>
+        </ul>
+    </div>
+</template>
+
+
+
