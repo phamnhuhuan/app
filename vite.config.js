d8e019cd09ab777e0854a3724370b0126b2308a4
baitapnhom12 <phuan3366@gmail.com>
2023-09-22 21:17:24 +0700

    app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    proxy: {
+      'huanapi.000webhostapp.com': 'huanapi.000webhostapp.com'
+    },
+    https: true,
+    port: 3306
+  },
   plugins: [vue()],
   base:'/app/'
 })
