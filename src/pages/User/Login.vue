fbefb0b77fcf5dfd790e85440dae1f374dae7d6a
baitapnhom12 <phuan3366@gmail.com>
2023-09-23 16:34:50 +0700

    app

--- src/pages/User/Login.vue
+++ src/pages/User/Login.vue
@@ -7,23 +7,17 @@
                     <i class="fa-solid fa-envelope icon_input"></i>
                     <input type="text" placeholder="Gmail" class=" flex-1" v-model="email">
                 </div>
-                <div class=" -my-5 text-error p-1" v-if="err_login.email">
-                <i class="fa-solid fa-circle-exclamation mx-2"></i>{{ err_login.email[0] }}
-                </div>
+                
                 <div class="auth_form my-5 sm:my-7">
                     <i class="fa-solid fa-unlock-keyhole icon_input"></i>
                     <input type="password" placeholder="Mật khẩu" class=" flex-1" v-model="password">
-                </div>
-                <div class=" -mt-5 mb-2 text-error p-1" v-if="err_login.password"><i class="fa-solid fa-circle-exclamation mx-2"></i>{{ err_login.password[0] }}</div>
+                </div> 
                 <div><button type="submit"
                         class="  w-full text-white text-center bg-primary py-2 rounded-[100px] hover:bg-hover-primary transition-all">Đăng
                         nhập</button></div>
-                        <div class=" mt-3 text-error" v-if="errors">
-                <i class="fa-solid fa-circle-exclamation mx-2"></i>{{ errors }}
-                </div>
                 <div class=" mt-3">Chưa có tài khoản ? <span
                         class=" text-primary cursor-pointer hover:text-hover-primary transition-all">
-                        <RouterLink :to="{name:'register'}">Đăng kí ngay</RouterLink>
+                        <RouterLink :to="{ name: 'register' }">Đăng kí ngay</RouterLink>
                     </span>
                 </div>
             </div>
@@ -31,8 +25,8 @@
     </div>
 </template>
 <script>
-import { reactive, toRefs, ref } from 'vue'
-import { UserStore} from "../../store/auth/UserStore";
+import {  reactive, toRefs, ref } from 'vue'
+import { UserStore } from "../../store/auth/UserStore";
 import { useRouter } from "vue-router";
 import { createToast } from 'mosha-vue-toastify';
 export default {
@@ -41,34 +35,33 @@ export default {
             email: '',
             password: '',
         })
-        const store=UserStore()
-        const errors=ref('')
+        const store = UserStore()
+        
         const router = useRouter()
-        const err_login = ref({})
+        
         const login = () => {
             axios.post('https://huanapi.000webhostapp.com/api/login', form)
                 .then((response) => {
                     if (response.data.status === false) {
-                        createToast('Sai tài khoản hoặc mật khẩu',{type:'danger',timeout:3000})
+                        createToast('Sai tài khoản hoặc mật khẩu', { type: 'danger', timeout: 3000 })
                     }
-                    else{
+                    else {
                         store.setToken(response.data.token)
                         router.push({ name: 'home' })
                     }
                     form.email = ''
                     form.password = ''
-                    err_login.value.email = ''
-                    err_login.value.password = ''
+                    
                 }).catch((error) => {
-                    err_login.value = error.response.data.errors
+                    
                     console.log(error)
                 });
         }
         return {
             store,
-            errors,
+            
             router,
-            err_login,
+            
             login,
             ...toRefs(form),
         }
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true,
-    proxy: {
-      'https://huanapi.000webhostapp.com': 'https://huanapi.000webhostapp.com'
-    },
-    https: true,
-    port: 3306
+host: true,
+// port: 8080,
+cors: false,
+https: true,
   },
-  plugins: [vue()],
+  plugins: [vue(),mkcert()],
   base:'/app/'
 })
